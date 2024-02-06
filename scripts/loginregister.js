00550a94162908432d130caff56eea5b7c4befa1
VizardAlpha <aurelien.199874@gmail.com>
2024-02-06 18:18:36 +0100

    Dernier changements
    Fix login/register form in french.
    Remove old user.png by a new user icon.png
    add animation menu link on all Attribut A in HTML.
    fix responsive menu list

--- scripts/loginregister.js
+++ scripts/loginregister.js
@@ -1,5 +1,6 @@
 let register_btn = document.querySelector(".Register-btn");
 let login_btn = document.querySelector(".Login-btn");
+
 let form = document.querySelector(".Form-box");
 register_btn.addEventListener("click", () => {
   form.classList.add("change-form");
